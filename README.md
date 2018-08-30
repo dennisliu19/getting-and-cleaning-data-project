f03a2c9c25c2c993655fca95e9f36e9047c24242
Haoyang Liu <haoyang@Haoyangs-MacBook-Pro.local>
2018-08-30 10:27:49 -0500

    upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# getting-and-cleaning-data-project
+# Getting and Cleaning Data - Course Project
+
+This is the folder for the course project of Getting and Cleaning Data on Courssera. The floder contains the following files
+
+* `tidydata.R` script that cleans the data
+* `tidydata.txt`: The final tidy data
+* `codebook`: Codebook for the final tidy data 
+
+#Clean Process
+
+1. Read data from the work directory (set if not set before)
+2. Load the corresponding activity and feature data
+3. Load test and train data. Filter out the designated data.
+4. Merge the dataset
+5. Convert the proper column into factors
+6. Creat a proper clean dataset
+
+
+
---
+++ tidydata.R
@@ -0,0 +1,41 @@
+install.packages("reshape2")
+library(reshape2)
+
+setwd("~/Desktop/coursera/UCI HAR Dataset")
+
+activity <- read.table("activity_labels.txt")
+activity[,2] <- as.character(activity[,2])
+features <- read.table("features.txt")
+features[,2] <- as.character(features[,2])
+
+
+# Extract only the data on mean and standard deviation
+remains <- grep(".*mean.*|.*std.*", features[,2])
+cleanname <- features[remains,2]
+cleanname = gsub('-mean', 'Mean', cleanname)
+cleanname = gsub('-std', 'Std', cleanname)
+cleanname <- gsub('[-()]', '', cleanname)
+
+# Load the datasets
+train <- read.table("train/X_train.txt")
+train <- train[remains]
+trainactivities <- read.table("train/Y_train.txt")
+trainsubjects <- read.table("train/subject_train.txt")
+train <- cbind(trainsubjects, trainactivities, train)
+
+test <- read.table("test/X_test.txt")
+test <- test[remains]
+testactivities <- read.table("test/Y_test.txt")
+testsubjects <- read.table("test/subject_test.txt")
+test <- cbind(testsubjects, testactivities, test)
+
+# merge datasets and add labels
+combined <- rbind(train, test)
+colnames(combined) <- c("subject", "activity", cleanname)
+combined$activity <- factor(combined$activity, levels = activity[,1], labels = activity[,2])
+combined$subject <- as.factor(combined$subject)
+
+clean.data <- melt(combined, id = c("subject", "activity"))
+clean.data <- dcast(clean.data, subject + activity ~ variable, mean)
+
+write.table(clean.data, "tidydata.txt")
